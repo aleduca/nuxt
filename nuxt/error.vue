b794c728243c25f73c1b4361c82f2dd8569306c4
Alexandre Cardoso <xandecar@hotmail.com>
2024-02-28 08:58:47 -0300

    Feat: error page

---
+++ nuxt/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>{{ error?.statusCode }}</h2>
+    <p>{{ error?.statusMessage }}</p>
+    <button @click="handleRedirect">Redirect to Home</button>
+  </div>
+</template>
+
+<script setup>
+// import type { NuxtError } from '#app'
+
+// const props = defineProps({
+//   error: Object as () => NuxtError
+// })
+
+const error = useError();
+
+function handleRedirect(){
+  return navigateTo('/');
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,8 +1,6 @@
 <template>
     <AppHeader />
 
-    <img src="/img/image1.jpeg" width="300" height="230">
-
     <div class="container">
         <slot />
     </div>
