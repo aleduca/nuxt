67f77f841cdcbcb5f570595edb38d203f9861f23
Alexandre Cardoso <xandecar@hotmail.com>
2024-03-06 13:54:58 -0300

    feat: useAsyncData

--- nuxt/pages/users/index.vue
+++ nuxt/pages/users/index.vue
@@ -2,6 +2,8 @@
   <div>
       <h2>Users</h2>
 
+      <button @click="execute">Fetch</button>
+
       <div v-if="error">
         {{ error.status }}
         {{ error.statusText }}
@@ -15,7 +17,7 @@
 
       <div v-else>
         <ul>
-          <li v-for="user in users">
+          <li v-for="user in data.users">
             {{ user.name }} - {{ user.email }}
           </li>
         </ul>
@@ -36,35 +38,24 @@ useHead({
   ],
 })
 
-const users = ref([]);
-const error = ref(null);
-const pending = ref(true);
-
 const config = useRuntimeConfig();
 
 // const endpoint= 'https://fakestoreapi.com/products';
-const endpoint= config.public.apiBase+'/api/users';
+const endpoint1= config.public.apiBase+'/api/users';
+const endpoint2= 'https://fakestoreapi.com/products/';
 
-async function loadUsers(){
-  try {
-    const data = await $fetch(endpoint);
-    pending.value = false;
+const {data,error,pending,execute} = await useAsyncData('users', async () => {
+  const [users, products] = await Promise.all([
+      $fetch(endpoint1),
+      $fetch(endpoint2)
+  ])
 
-    users.value = data;
-  } catch (e) {
-    error.value = e;
-    console.log(e)
-  }
-
-}
-// console.log(users);
-
-onMounted(() => {
-  nextTick(() => {
-    loadUsers();
-  });
+  return {users, products}
+},{
+  lazy: true,
 })
 
+console.log(data.value.products);
 
 </script>
 
